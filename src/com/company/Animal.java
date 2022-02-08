c952cd04135163667f820e864ad3efaa7c437f9c
n.khaimuldin <n.khaimuldin@astanait.edu.kz>
2022-02-08 16:19:38 +0600

    Implemented method makeStep - kjf;ldsknf;dsklsdlmd'v,

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -14,7 +14,9 @@ public abstract class Animal {
         return name;
     }
     public abstract void makeSound(); //abstract method without any implementation
-
+    public void makeStep(){
+        System.out.println("Made step");
+    }
     @Override
     public String toString() {
         return "Animal name " +name;
